3f213b60df5b6c9f94439ad8934b60e7cf798e94
zivbitton <ziv3223@gmail.com>
2023-09-06 13:40:08 +0300

    new file yoav

--- yoavv.tf
+++ yoavv.tf
@@ -1,48 +1,13 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "3.53.0"
-    }
-  }
+
+provider "aws" {
+  region = "us-east-1" 
 }
 
-provider "google" {
-  credentials = file("path/to/your/gcp/credentials.json") # ציין את נתיב קובץ הכרטיסים שלך
-  project     = "your-gcp-project-id" # ציין את זיהוי הפרויקט שלך ב-GCP
-  region      = "us-central1" # ציין את האזור הרצוי של GCP
-}
+# הגדרת מכונת EC2
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t3.micro" 
 
-resource "google_container_cluster" "example_cluster" {
-  name     = "example-cluster"
-  location = "us-central1" 
 
-  initial_node_count = 3 # כמות הסרברים הראשונים בקלאסטר
-
-  node_config {
-    machine_type = "e2-standard-2" 
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-
-  master_auth {
-    username = ""
-    password = ""
-  }
-
-  maintenance_policy {
-    daily_maintenance_window {
-      start_time = "03:00"
-    }
-  }
 }
 
